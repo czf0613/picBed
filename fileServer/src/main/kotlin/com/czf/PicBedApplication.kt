5f53ae871e50758f78bfcfde274d2bc4e2188666
czf <1354016594@qq.com>
2020-10-06 20:21:17 +0800

    注册了新域名
    使用了专门的域名来提供服务

--- fileServer/src/main/kotlin/com/czf/PicBedApplication.kt
+++ fileServer/src/main/kotlin/com/czf/PicBedApplication.kt
@@ -11,7 +11,7 @@ class PicBedApplication
 
 const val itemPath = "/picBed/userFiles"
 const val iconPath = "/picBed/icons"
-const val domain = "https://moral-helper.online:23456"
+const val domain = "https://domain.xxx:23456"
 const val defaultImage = "/picBed/icons/default.png"
 
 fun main(args: Array<String>) {
